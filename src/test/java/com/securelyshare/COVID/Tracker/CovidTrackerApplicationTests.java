a105218fbd6cfba821d2c9f4374499b4679bc89e
porontonel <anish.shaw@securelyshare.com>
2022-03-21 17:16:26 +0530

    First commit

---
+++ src/test/java/com/securelyshare/COVID/Tracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securelyshare.COVID.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
